1d047e62adfa4347f4d6b9c0d6746afcf2e11774
RobertSmithers <rjsmithers3@gmail.com>
2024-12-05 23:48:45 -0600

    Expand daily setup with empty example file and template py file

--- setupForDay.py
+++ setupForDay.py
@@ -9,11 +9,21 @@ args = parser.parse_args()
 
 day = args.day
 
-data = get_data(day=day, year=2024, block=True)
-
 directory = f'day{day}'
+template_path_out = os.path.join(directory, 'part1.py')
+
 if not os.path.exists(directory):
     os.makedirs(directory)
 
+data = get_data(day=day, year=2024, block=True)
+
 with open(os.path.join(directory, 'input.txt'), 'w') as f:
     f.write(data)
+
+with open(os.path.join(directory, 'example.txt'), 'w') as f:
+    pass
+
+
+if os.path.exists('template.py') and not os.path.exists(template_path_out):
+    with open('template.py', 'r') as src, open(template_path_out, 'w') as dst:
+        dst.write(src.read())
