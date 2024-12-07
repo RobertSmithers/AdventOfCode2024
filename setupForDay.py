36d179b0db720c92075111224bb00db27ab722fb
RobertSmithers <rjsmithers3@gmail.com>
2024-12-06 23:27:05 -0600

    Fix daily setup

--- setupForDay.py
+++ setupForDay.py
@@ -15,15 +15,14 @@ template_path_out = os.path.join(directory, 'part1.py')
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-data = get_data(day=day, year=2024, block=True)
-
-with open(os.path.join(directory, 'input.txt'), 'w') as f:
-    f.write(data)
+if os.path.exists('template.py') and not os.path.exists(template_path_out):
+    with open('template.py', 'r') as src, open(template_path_out, 'w') as dst:
+        dst.write(src.read())
 
 with open(os.path.join(directory, 'example.txt'), 'w') as f:
     pass
 
+data = get_data(day=day, year=2024, block=True)
 
-if os.path.exists('template.py') and not os.path.exists(template_path_out):
-    with open('template.py', 'r') as src, open(template_path_out, 'w') as dst:
-        dst.write(src.read())
+with open(os.path.join(directory, 'input.txt'), 'w') as f:
+    f.write(data)
