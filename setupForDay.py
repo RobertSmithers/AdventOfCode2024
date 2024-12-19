585e0d4329553d10cc60d118618cce7c9d67d3c6
RobertSmithers <rjsmithers3@gmail.com>
2024-12-18 23:24:36 -0600

    Update setup to retry pull after time-sync failures

--- setupForDay.py
+++ setupForDay.py
@@ -1,7 +1,7 @@
-from aocd import get_data, submit
-from aoc.helpers import input_lines
+from aocd import get_data
 import argparse
 import os
+import time
 
 parser = argparse.ArgumentParser(description='Advent of Code Day Setup')
 parser.add_argument('day', type=int, help='Day of the Advent of Code')
@@ -28,3 +28,9 @@ try:
         f.write(data)
 except KeyboardInterrupt:
     print('\nExiting...')
+except Exception as e:
+    # Catch the time-based failure and repull
+    time.sleep(1)
+    data = get_data(day=day, year=2024, block=True)
+    with open(os.path.join(directory, 'input.txt'), 'w') as f:
+        f.write(data)
